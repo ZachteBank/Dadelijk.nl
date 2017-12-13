583e2e72ca2cb1ded9fa9a82c2ceb334092944db
bramk <bramkempen@gmail.com>
2017-12-13 20:22:05 +0100

    fix duplicate code

--- DataAccess/Repositories/BaseRepository.cs
+++ DataAccess/Repositories/BaseRepository.cs
@@ -34,18 +34,24 @@ namespace DataAccess.Repositories
 
         protected void SetIdOfModel(object model, int id)
         {
-            var t = model.GetType();
-            if (t.GetProperty("Id", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance) == null)
-                throw new ArgumentOutOfRangeException(nameof(model), string.Format("Property {0} was not found in Type {1}", "Id", model.GetType().FullName));
-            t.InvokeMember("Id", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.FlattenHierarchy, null, model, new object[] { id });
+            SetPropertyOfModel(model, id, "Id");
         }
 
         protected void SetDateUpdatedOfModel(object model, DateTime dateTime)
         {
+            SetPropertyOfModel(model, dateTime, "DateUpdated");
+        }
+
+        private void SetPropertyOfModel(object model, object value, string property)
+        {
             var t = model.GetType();
-            if (t.GetProperty("DateUpdated", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance) == null)
-                throw new ArgumentOutOfRangeException(nameof(model), string.Format("Property {0} was not found in Type {1}", "DateUpdated", model.GetType().FullName));
-            t.InvokeMember("DateUpdated", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.FlattenHierarchy, null, model, new object[] { dateTime });
+            if (t.GetProperty(property, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance) == null)
+                throw new ArgumentOutOfRangeException(nameof(model), string.Format("Property {0} was not found in Type {1}", property, model.GetType().FullName));
+            t.InvokeMember(property, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.FlattenHierarchy, null, model, new object[] { value });
+            /*var t = model.GetType();
+            if (t.GetProperty("Id", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance) == null)
+                throw new ArgumentOutOfRangeException(nameof(model), string.Format("Property {0} was not found in Type {1}", "Id", model.GetType().FullName));
+            t.InvokeMember("Id", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.FlattenHierarchy, null, model, new object[] { id });*/
         }
     }
 }
